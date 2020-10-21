c257da70b0b03af6cd64157f881e8fd07ac0360c
Abhishek Upadhyay <aupadhyay@qubole.com>
2020-10-21 14:24:30 +0530

    changes

--- src/components/home/MyNavbar.js
+++ src/components/home/MyNavbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import { BsFillXDiamondFill, BsBriefcase, BsPhone } from "react-icons/bs";
 
 const MyNavbar = () => {
     return (
-        <Navbar>
+        <Navbar expand="lg">
             <Navbar.Brand href="#home">
                 <img src="https://drive.google.com/uc?id=18mfqWkOV2BvCil3qWlVXcsh0xlE6GQA-" className="logo" alt="Abhishek Kumar Upadhyay"></img>
             </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end">
-                <div className="ml-auto pr-3 nav-links">
-                    <a href="#workeperience"><BsBriefcase /> Work Experiences</a>
-                    <a href="#skills"><BsFillXDiamondFill /> Skills</a>
-                    <a href="#contact"><BsPhone /> Contact</a>
-                </div>
+                <Nav className="ml-auto pr-3 pt-3 nav-links">
+                    <Nav.Link href="#workeperience" className="px-4" style={{ color:'#394867'}}><BsBriefcase /> Work Experiences</Nav.Link>
+                    <Nav.Link href="#skills" className="px-4" style={{ color: '#394867' }}><BsFillXDiamondFill /> Skills</Nav.Link>
+                    <Nav.Link href="#contact" className="px-4 mr-5" style={{ color: '#394867' }}><BsPhone /> Contact</Nav.Link>
+                </Nav>
             </Navbar.Collapse>
         </Navbar>
     )
