9e6f722aec671d747a797882d9901b807ce2868d
Abhishek Upadhyay <aupadhyay@qubole.com>
2020-10-23 18:38:47 +0530

    adding navbar collapse

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { BsFillXDiamondFill, BsBriefcase, BsPhone, BsViewList, BsDownload } from "react-icons/bs";
 import { Link } from "react-scroll";
 
 const MyNavbar = () => {
+    const [expanded, setExpanded] = useState(false)
+
+    const setNavExpanded = () => {
+        setExpanded(!expanded);
+    };
+    const closeNav = () => {
+        setExpanded(false);
+    }
+
     return (
-        <Navbar expand="lg" fixed="top" className="navbar-section">
+        <Navbar expand="lg" fixed="top" className="navbar-section" onToggle={setNavExpanded}
+            expanded={expanded}>
             <Navbar.Brand>
                 <Link to="home" spy={true} smooth={true} duration={500} offset={-60}>
-                    <img src="https://drive.google.com/uc?id=18mfqWkOV2BvCil3qWlVXcsh0xlE6GQA-" className="logo" alt="Abhishek Kumar Upadhyay" />
+                    <img src="https://drive.google.com/uc?id=18mfqWkOV2BvCil3qWlVXcsh0xlE6GQA-" className="logo" alt="Abhishek" />
                 </Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <BsViewList className="menu-icon" />
             </Navbar.Toggle>
             <Navbar.Collapse className="justify-content-end">
-                <Nav className="ml-auto nav-links">
-                    <Link to="workexperience" spy={true} smooth={true} duration={500} offset={-60}><BsBriefcase /> Experiences</Link>
-                    <Link to="skills" spy={true} smooth={true} duration={500} offset={-60}><BsFillXDiamondFill /> Skills</Link>
-                    <Link to="contact" spy={true} smooth={true} duration={500} offset={-60}><BsPhone /> Contact Me</Link>
+                <Nav className="ml-auto nav-links" onClick={closeNav}>
+                    <Link to="workexperience" onClick={closeNav} spy={true} smooth={true} duration={500} offset={-60}><BsBriefcase /> Experiences</Link>
+                    <Link to="skills" onClick={closeNav} spy={true} smooth={true} duration={500} offset={-60}><BsFillXDiamondFill /> Skills</Link>
+                    <Link to="contact" onClick={closeNav} spy={true} smooth={true} duration={500} offset={-60}><BsPhone /> Contact Me</Link>
                     <a href="https://drive.google.com/uc?export=download&id=13QeYjwjoohqTza4QmpgD29Pcp3zJ97d6"><BsDownload /> Resume</a>
                 </Nav>
             </Navbar.Collapse>
